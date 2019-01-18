f285d0bd8f740a8ba6c70dc6bdd142f00495b13f
joshi-chinmay <chinmayj93@gmail.com>
2019-01-18 22:08:34 +0800

    updated and improved carouse

---
+++ src/components/PortfolioCarousel.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import '../styles/OverrideCarousel.scss';
+import { Carousel } from 'react-responsive-carousel';
+
+class PortfolioCarousel extends Component {
+
+  createImages() {
+    var arr = [];
+
+    for (let i = 0; i < images.length; i++) {
+      arr.push(
+        <div key={i}>
+          <img src={images[i]} alt="portfolio-work-images" />
+        </div>
+      )
+    }
+    return arr;
+  }
+
+  render() {
+    return (
+      <Carousel showIndicators={false} useKeyboardArrows={true}>
+        {this.createImages()}
+      </Carousel>
+    );
+  }
+}
+
+function importAll(r) {
+  var arr = [];
+  for(var i = 4; i < 50; i++) {
+    arr.push("./Slide" + i + ".JPG");
+  }
+  return arr.map(r);
+}
+
+const images = importAll(require.context('../images/portfolio-images/', true, /\.(JPG)$/));
+export default PortfolioCarousel;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar.jsx';
 import '../styles/Work.scss';
+import PortfolioCarousel from './PortfolioCarousel.jsx';
 
 class Work extends Component {
-
-  createImages() {
-    var arr = [];
-    for (let i = 0; i < images.length; i++) {
-      arr.push(
-        <div className={"carousel-item text-center " + (i === 0 ? "active" : "")} key={i}>
-          <img src={images[i]} alt="portfolio-work-images" />
-        </div>
-      )
-    }
-    return arr;
-  }
 
   render() {
     return (
@@ -22,30 +11,15 @@ class Work extends Component {
         <Navbar />
 
         <div className="row">
-          <div className="col-md-12">
-            <div id="portfolio-carousel" className="my-3 carousel slide" data-ride="carousel">
-              <div className="carousel-inner">
-                {this.createImages()}
-              </div>
-              <a className="carousel-control-prev" href="#portfolio-carousel" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a className="carousel-control-next" href="#portfolio-carousel" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-              </a>
-            </div>
+          <div className="col-xs-12 col-md-2"></div>
+          <div className="col-md-8">
+            <PortfolioCarousel />
           </div>
+          <div className="col-xs-12 col-md-2"></div>
         </div>
       </div>
     );
   }
 }
 
-function importAll(r) {
-  return r.keys().map(r);
-}
-
-const images = importAll(require.context('../images/portfolio-images/', false, /\.(JPG)$/));
 export default Work;
