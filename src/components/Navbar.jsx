efc447702f540a659db93a5556e57325ef66662e
joshi-chinmay <chinmayj93@gmail.com>
2019-10-02 09:35:48 -0400

    added resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../styles/Navbar.scss';
+import ResumeLink from '../docs/KJ-Resume 2019.pdf';
 
 class Navbar extends Component {
   render() {
@@ -14,7 +15,7 @@ class Navbar extends Component {
           <Link to="/about" className="navigation-link">About</Link>
           <Link to="/work" className="navigation-link">Work</Link>
           <Link to="/sketchpressions" className="navigation-link">Sketchpressions</Link>
-          <a href="https://drive.google.com/open?id=1_WRsssugWmUJcBgL199ntCI9yPOg4FLP" target="_blank" className="navigation-link">Resume</a>
+          <a href={ResumeLink} target="_blank" className="navigation-link">Resume</a>
           <a href="https://drive.google.com/open?id=0BypWPAmJdsyuMlV3TUpiRFAyaS0ybW1wT1hXOEQ4X3BPNDZ3" target="_blank" className="navigation-link">Architecture Portfolio</a>
         </div>
         <div className="col-xs-12 col-sm-12 col-md-1">
